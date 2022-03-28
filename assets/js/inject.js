18936560009c7f7d0b27a1e23d77e9348d6ad7d3
hsddev <75630291+hsddev@users.noreply.github.com>
2022-03-28 15:18:15 +0100

    Update inject.js
    last fix

--- assets/js/inject.js
+++ assets/js/inject.js
@@ -2,52 +2,66 @@ document.body.innerHTML += `
 <noscript><img height="1" width="1" style="display:none"
 src="https://www.facebook.com/tr?id=2942291289344991&ev=PageView&noscript=1"
 /></noscript>
-`
+`;
 
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '2942291289344991');
-fbq('track', 'PageView');
+!(function (f, b, e, v, n, t, s) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+        n.callMethod
+            ? n.callMethod.apply(n, arguments)
+            : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = "2.0";
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s);
+})(
+    window,
+    document,
+    "script",
+    "https://connect.facebook.net/en_US/fbevents.js"
+);
+fbq("init", "2942291289344991");
+fbq("track", "PageView");
 
-document.body.innerHTML += '<script async src="https://www.googletagmanager.com/gtag/js?id=G-FLXP00HX2X"></script>'
+document.body.innerHTML +=
+    '<script async src="https://www.googletagmanager.com/gtag/js?id=G-FLXP00HX2X"></script>';
 window.dataLayer = window.dataLayer || [];
 
 function gtag() {
-  dataLayer.push(arguments);
+    dataLayer.push(arguments);
 }
-gtag('js', new Date());
+gtag("js", new Date());
 
-gtag('config', 'G-FLXP00HX2X');
+gtag("config", "G-FLXP00HX2X");
 
 if (location.pathname === "/") {
+    const rightContent = document.querySelector(".right");
+    const textContent = document.querySelector(".text");
+    const contactContent = document.querySelector(".contact");
 
-  const rightContent = document.querySelector('.right')
-  const textContent = document.querySelector('.text')
-  const contactContent = document.querySelector('.contact')
-
-  const htmlVideoString = `
+    const htmlVideoString = `
   <iframe width="500" height="300" src="https://www.youtube.com/embed/0CWzFx1HRvA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="margin-bottom: 100px;"></iframe>
-`
+`;
 
-  rightContent.insertAdjacentHTML('afterbegin', htmlVideoString)
-  
-  
-  // Changing the color of the sign in button
-if (document.querySelector("#sign-in")) {
-    signIn.style.background = "#56C271"
-    signIn.style.color = "hsl(0, 0%, 8%)"
-  
-  const titleString = `
+    rightContent.insertAdjacentHTML("afterbegin", htmlVideoString);
+
+    // Changing the color of the sign in button
+    if (document.querySelector("#sign-in")) {
+        signIn.style.background = "#56C271";
+        signIn.style.color = "hsl(0, 0%, 8%)";
+
+        const titleString = `
     Welcome to <span style="color: #56C271">01 Founders!</span>
-`
+`;
 
-  const contentString = `
+        const contentString = `
         <span id="zone-01">Zone 01 is our collaborative coding education platform.</span>
         <br><br>
         <span id="zone-01-process">New here? Here’s what you need to do:</span>
@@ -77,10 +91,9 @@ if (document.querySelector("#sign-in")) {
         <h3>This is where the challenge begins. Zzle tests your logic and perseverance. You'll be lead through a series of puzzles and your job is to figure out how to move the cursor to the end. 
         Experiment with different possibilities and see how quickly you can get through.</h3>  </span> 
         
-        `
+        `;
 
-
-  const contactString = `
+        const contactString = `
 
       <div id="about-us">Want to know more about 01 Founders, London's new free coding school, with a job guarantee?&thinsp;</div>
       <div class="contact">
@@ -100,14 +113,11 @@ if (document.querySelector("#sign-in")) {
         <span class="arrow">→</span>
         <span id="contact">Contact us.</span>
       </a>
-      </div>`
+      </div>`;
 
+        textContent.innerHTML = contentString;
 
-  textContent.innerHTML = contentString
-
-  contactContent.insertAdjacentHTML('afterend', contactString)
-  contactContent.remove()
-
+        contactContent.insertAdjacentHTML("afterend", contactString);
+        contactContent.remove();
+    }
 }
-  
-  
